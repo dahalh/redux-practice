e035d3f91c957bf29e84dfdcf424daad5532f91e
dahalh <dahalh@outlook.com>
2022-11-27 21:22:06 +1100

    post form working

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: "0", name: "Dude Lebowski" },
+  { id: "1", name: "Niel Young" },
+  { id: "2", name: "Dave Gray" },
+];
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+});
+
+export const selectAllUsers = (state) => state.usersSlice;
+
+export default usersSlice.reducer;
